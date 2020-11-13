5425b1032c05ec78b65e4929faa32c0accfe4e06
K <kayo@illumium.org>
2020-11-14 03:55:49 +0500

    Added iterator for array and missing set method

--- src/value/array.rs
+++ src/value/array.rs
@@ -1,8 +1,12 @@
 use crate::{
     value::{self, rf::JsObjectRef},
-    Ctx, FromJs, Object, Result, Value,
+    Ctx, FromJs, Object, Result, ToJs, Value,
 };
 use rquickjs_sys as qjs;
+use std::{
+    iter::{IntoIterator, Iterator},
+    marker::PhantomData,
+};
 
 /// Rust representation of a javascript object optimized as an array.
 ///
@@ -44,13 +48,76 @@ impl<'js> Array<'js> {
         self.len() == 0
     }
 
-    /// Get the value at a index in the javascript array.
+    /// Get the value at an index in the javascript array.
     pub fn get<V: FromJs<'js>>(&self, idx: u32) -> Result<V> {
+        let obj = self.0.as_js_value();
+        let val = unsafe {
+            let val = qjs::JS_GetPropertyUint32(self.0.ctx.ctx, obj, idx);
+            Value::from_js_value(self.0.ctx, val)
+        }?;
+        V::from_js(self.0.ctx, val)
+    }
+
+    /// Set the value at an index in the javascript array.
+    pub fn set<V: ToJs<'js>>(&self, idx: u32, val: V) -> Result<()> {
+        let obj = self.0.as_js_value();
+        let val = val.to_js(self.0.ctx)?.into_js_value();
         unsafe {
-            let v = self.0.as_js_value();
-            let val = qjs::JS_GetPropertyUint32(self.0.ctx.ctx, v, idx);
-            let val = Value::from_js_value(self.0.ctx, val)?;
-            V::from_js(self.0.ctx, val)
+            if -1 == qjs::JS_SetPropertyUint32(self.0.ctx.ctx, obj, idx, val) {
+                return Err(value::get_exception(self.0.ctx));
+            }
+        }
+        Ok(())
+    }
+
+    /// Get iterator over elments of an array
+    pub fn iter<T: FromJs<'js>>(&self) -> ArrayIter<'js, T> {
+        let count = self.len() as u32;
+        ArrayIter {
+            array: self.clone(),
+            index: 0,
+            count,
+            marker: PhantomData,
+        }
+    }
+}
+
+/// The iterator for an array
+pub struct ArrayIter<'js, T> {
+    array: Array<'js>,
+    index: u32,
+    count: u32,
+    marker: PhantomData<T>,
+}
+
+impl<'js, T> Iterator for ArrayIter<'js, T>
+where
+    T: FromJs<'js>,
+{
+    type Item = Result<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index < self.count {
+            let res = self.array.get(self.index);
+            self.index += 1;
+            Some(res)
+        } else {
+            None
+        }
+    }
+}
+
+impl<'js> IntoIterator for Array<'js> {
+    type Item = Result<Value<'js>>;
+    type IntoIter = ArrayIter<'js, Value<'js>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let count = self.len() as u32;
+        ArrayIter {
+            array: self,
+            index: 0,
+            count,
+            marker: PhantomData,
         }
     }
 }
@@ -100,6 +167,47 @@ mod test {
                 .unwrap();
             let object = val.into_object();
             assert_eq!(object.get::<_, i32>(0).unwrap(), 1);
+        })
+    }
+
+    #[test]
+    fn into_iter() {
+        let rt = Runtime::new().unwrap();
+        let ctx = Context::full(&rt).unwrap();
+        ctx.with(|ctx| {
+            let val: Array = ctx
+                .eval(
+                    r#"
+                      [1,'abcd',true]
+                    "#,
+                )
+                .unwrap();
+            let elems: Vec<_> = val.into_iter().collect::<Result<_>>().unwrap();
+            assert_eq!(elems.len(), 3);
+            assert_eq!(elems[0], Value::Int(1));
+            assert_eq!(StdString::from_js(ctx, elems[1].clone()).unwrap(), "abcd");
+            assert_eq!(elems[2], Value::Bool(true));
+        })
+    }
+
+    #[test]
+    fn iter() {
+        let rt = Runtime::new().unwrap();
+        let ctx = Context::full(&rt).unwrap();
+        ctx.with(|ctx| {
+            let val: Array = ctx
+                .eval(
+                    r#"
+                      ["a", 'b', '', "cdef"]
+                    "#,
+                )
+                .unwrap();
+            let elems: Vec<StdString> = val.iter().collect::<Result<_>>().unwrap();
+            assert_eq!(elems.len(), 4);
+            assert_eq!(elems[0], "a");
+            assert_eq!(elems[1], "b");
+            assert_eq!(elems[2], "");
+            assert_eq!(elems[3], "cdef");
         })
     }
 }
