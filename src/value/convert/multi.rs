f7efaac7094b4b19aed1fab9c24b20ae43050031
K <kayo@illumium.org>
2020-11-15 13:50:35 +0500

    Added tests for var-args passing

--- src/value/convert/multi.rs
+++ src/value/convert/multi.rs
@@ -158,12 +158,120 @@ impl_from_to_js_multi! {
 
 #[cfg(test)]
 mod test {
-    use super::*;
+    use crate::*;
 
     #[test]
     fn from_js_multi_len() {
         assert_eq!(<((),)>::LEN, 1);
         assert_eq!(<((), ())>::LEN, 2);
         assert_eq!(<((), (), ())>::LEN, 3);
+    }
+
+    #[test]
+    fn call_js_fn_with_var_args() {
+        let rt = Runtime::new().unwrap();
+        let ctx = Context::full(&rt).unwrap();
+        let res: Vec<i8> = ctx.with(|ctx| {
+            let func: Function = ctx
+                .eval(
+                    r#"
+                  (...x) => [x.length, ...x]
+                "#,
+                )
+                .unwrap();
+            func.call(RestValues::from(vec![1, 2, 3])).unwrap()
+        });
+        assert_eq!(res.len(), 4);
+        assert_eq!(res[0], 3);
+        assert_eq!(res[1], 1);
+        assert_eq!(res[2], 2);
+        assert_eq!(res[3], 3);
+    }
+
+    #[test]
+    fn call_js_fn_with_rest_args() {
+        let rt = Runtime::new().unwrap();
+        let ctx = Context::full(&rt).unwrap();
+        let res: Vec<i8> = ctx.with(|ctx| {
+            let func: Function = ctx
+                .eval(
+                    r#"
+                  (a, b, ...x) => [a, b, x.length, ...x]
+                "#,
+                )
+                .unwrap();
+            func.call((-2, -1, RestValues::from(vec![1, 2]))).unwrap()
+        });
+        assert_eq!(res.len(), 5);
+        assert_eq!(res[0], -2);
+        assert_eq!(res[1], -1);
+        assert_eq!(res[2], 2);
+        assert_eq!(res[3], 1);
+        assert_eq!(res[4], 2);
+    }
+
+    #[test]
+    fn call_rust_fn_with_var_args() {
+        let rt = Runtime::new().unwrap();
+        let ctx = Context::full(&rt).unwrap();
+        let res: Vec<i8> = ctx.with(|ctx| {
+            let func = Function::new(
+                ctx,
+                "test_fn",
+                |_ctx, _this: Value, args: RestValues<i8>| -> Result<_> {
+                    use std::iter::once;
+                    Ok(once(args.len() as i8)
+                        .chain(args.iter().cloned())
+                        .collect::<Vec<_>>())
+                },
+            )
+            .unwrap();
+            ctx.globals().set("test_fn", func).unwrap();
+            ctx.eval(
+                r#"
+                  test_fn(1, 2, 3)
+                "#,
+            )
+            .unwrap()
+        });
+        assert_eq!(res.len(), 4);
+        assert_eq!(res[0], 3);
+        assert_eq!(res[1], 1);
+        assert_eq!(res[2], 2);
+        assert_eq!(res[3], 3);
+    }
+
+    #[test]
+    fn call_rust_fn_with_rest_args() {
+        let rt = Runtime::new().unwrap();
+        let ctx = Context::full(&rt).unwrap();
+        let res: Vec<i8> = ctx.with(|ctx| {
+            let func = Function::new(
+                ctx,
+                "test_fn",
+                |_ctx, _this: Value, (arg1, arg2, args): (i8, i8, RestValues<i8>)| -> Result<_> {
+                    use std::iter::once;
+                    Ok(once(arg1)
+                        .chain(once(arg2))
+                        .chain(once(args.len() as i8))
+                        .chain(args.iter().cloned())
+                        .collect::<Vec<_>>())
+                },
+            )
+            .unwrap();
+            ctx.globals().set("test_fn", func).unwrap();
+            ctx.eval(
+                r#"
+                  test_fn(-2, -1, 1, 2)
+                "#,
+            )
+            .unwrap()
+        });
+        assert_eq!(res.len(), 5);
+        assert_eq!(res[0], -2);
+        assert_eq!(res[1], -1);
+        assert_eq!(res[2], 2);
+        assert_eq!(res[3], 1);
+        assert_eq!(res[4], 2);
     }
 }
