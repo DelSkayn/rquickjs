f623b82629b819d5d31913bb833301d509750c48
K <kayo@illumium.org>
2020-11-25 11:03:22 +0500

    Simplified string conversion

--- src/value/string.rs
+++ src/value/string.rs
@@ -4,7 +4,7 @@ use crate::{
     value::{self, rf::JsStringRef},
     Error, Result,
 };
-use std::{ffi::CStr, mem, string::String as StdString};
+use std::{mem, slice, str, string::String as StdString};
 
 /// Rust representation of a javascript string.
 #[derive(Debug, Clone, PartialEq)]
@@ -13,40 +13,30 @@ pub struct String<'js>(pub(crate) JsStringRef<'js>);
 impl<'js> String<'js> {
     /// Convert the javascript string to a rust string.
     pub fn to_string(&self) -> Result<StdString> {
-        pub struct DropStr<'js>(Ctx<'js>, *const i8);
-
-        impl<'js> Drop for DropStr<'js> {
-            fn drop(&mut self) {
-                unsafe {
-                    qjs::JS_FreeCString(self.0.ctx, self.1);
-                }
-            }
+        let mut len = mem::MaybeUninit::uninit();
+        let ptr =
+            unsafe { qjs::JS_ToCStringLen(self.0.ctx.ctx, len.as_mut_ptr(), self.0.as_js_value()) };
+        if ptr.is_null() {
+            // Might not ever happen but I am not 100% sure
+            // so just incase check it.
+            return Err(Error::Unknown);
         }
-
-        unsafe {
-            let c_str = qjs::JS_ToCString(self.0.ctx.ctx, self.0.as_js_value());
-            // Ensure the c_string is dropped no matter what happens
-            let drop = DropStr(self.0.ctx, c_str);
-            if c_str.is_null() {
-                // Might not ever happen but I am not 100% sure
-                // so just incase check it.
-                return Err(Error::Unknown);
-            }
-            let res = CStr::from_ptr(c_str).to_str()?.to_string();
-            mem::drop(drop);
-            Ok(res)
-        }
+        let len = unsafe { len.assume_init() };
+        let bytes: &[u8] = unsafe { slice::from_raw_parts(ptr as _, len as _) };
+        let result = str::from_utf8(bytes).map(|s| s.into());
+        unsafe { qjs::JS_FreeCString(self.0.ctx.ctx, ptr) };
+        result.map_err(Error::from)
     }
 
     /// Create a new js string from an rust string.
     pub fn from_str(ctx: Ctx<'js>, s: &str) -> Result<Self> {
-        unsafe {
-            let len = s.len();
-            let bytes = s.as_ptr() as *const i8;
-            let js_val = qjs::JS_NewStringLen(ctx.ctx, bytes, len as _);
+        let len = s.as_bytes().len();
+        let ptr = s.as_ptr();
+        Ok(String(unsafe {
+            let js_val = qjs::JS_NewStringLen(ctx.ctx, ptr as _, len as _);
             let js_val = value::handle_exception(ctx, js_val)?;
-            Ok(String(JsStringRef::from_js_value(ctx, js_val)))
-        }
+            JsStringRef::from_js_value(ctx, js_val)
+        }))
     }
 }
 
