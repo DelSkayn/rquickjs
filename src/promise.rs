44856e489960d4d1b7c10f53c98638d53b420958
K <kayo@illumium.org>
2020-11-28 11:17:48 +0500

    Updated `PromiseJs` wrapper

--- src/promise.rs
+++ src/promise.rs
@@ -1,24 +1,31 @@
+#[cfg(feature = "deferred-resolution")]
+use crate::qjs;
 use crate::{
-    qjs, Ctx, Error, FromJs, Function, IntoJs, Object, Result, SendWhenParallel, StdResult, This,
-    Value,
+    safe_ref::Ref, Ctx, Error, FromJs, Function, IntoJs, JsFn, Object, Persistent, Result,
+    SendWhenParallel, This, Value,
 };
 use std::{
-    fmt::Display,
     future::Future,
     mem,
     pin::Pin,
-    sync::{Arc, Mutex},
     task::{Context, Poll, Waker},
 };
 
 /// Future-aware promise
 pub struct Promise<T> {
-    state: Arc<Mutex<State<T>>>,
+    state: Ref<State<T>>,
 }
 
 struct State<T> {
     result: Option<Result<T>>,
     waker: Option<Waker>,
+}
+
+impl<T> State<T> {
+    fn resolve(&mut self, result: Result<T>) {
+        self.result = Some(result);
+        self.waker.take().map(|waker| waker.wake());
+    }
 }
 
 impl<T> Default for State<T> {
@@ -37,27 +44,21 @@ where
     fn from_js(ctx: Ctx<'js>, value: Value<'js>) -> Result<Self> {
         let obj = Object::from_js(ctx, value)?;
         let then: Function = obj.get("then")?;
-        let state = Arc::new(Mutex::new(State::default()));
-        let on_ok = Function::new(ctx, "onSuccess", {
+        let state = Ref::new(State::default());
+        let on_ok = JsFn::new("onSuccess", {
             let state = state.clone();
             move |ctx: Ctx<'js>, value: Value<'js>| {
-                let mut state = state.lock().unwrap();
-                state.result = Some(T::from_js(ctx, value));
-                if let Some(waker) = state.waker.take() {
-                    waker.wake();
-                }
+                let mut state = state.lock();
+                state.resolve(T::from_js(ctx, value));
             }
-        })?;
-        let on_err = Function::new(ctx, "onError", {
+        });
+        let on_err = JsFn::new("onError", {
             let state = state.clone();
             move |error: Error| {
-                let mut state = state.lock().unwrap();
-                state.result = Some(Err(error));
-                if let Some(waker) = state.waker.take() {
-                    waker.wake();
-                }
+                let mut state = state.lock();
+                state.resolve(Err(error));
             }
-        })?;
+        });
         then.call((This(obj), on_ok, on_err))?;
         Ok(Self { state })
     }
@@ -67,7 +68,7 @@ impl<T> Future for Promise<T> {
     type Output = Result<T>;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let mut state = self.state.lock().unwrap();
+        let mut state = self.state.lock();
         if let Some(result) = state.result.take() {
             return Poll::Ready(result);
         }
@@ -87,11 +88,10 @@ impl<T> From<T> for PromiseJs<T> {
 }
 
 #[cfg(any(feature = "async-std", feature = "tokio"))]
-impl<'js, 'a, T, V, E> IntoJs<'js> for PromiseJs<T>
+impl<'js, T> IntoJs<'js> for PromiseJs<T>
 where
-    T: Future<Output = StdResult<V, E>> + 'static,
-    V: IntoJs<'js> + 'static,
-    E: Display + 'static,
+    T: Future + 'static,
+    T::Output: IntoJs<'js> + 'static,
 {
     fn into_js(self, ctx: Ctx<'js>) -> Result<Value<'js>> {
         #[cfg(feature = "async-std")]
@@ -101,8 +101,8 @@ where
 
         let (promise, then, catch) = ctx.promise()?;
 
-        let then = ctx.register(Value::Function(then));
-        let catch = ctx.register(Value::Function(catch));
+        let then = Persistent::save(ctx, then)?.outlive();
+        let catch = Persistent::save(ctx, catch)?.outlive();
 
         let runtime = unsafe { &ctx.get_opaque().runtime }
             .try_ref()
@@ -115,17 +115,21 @@ where
             let result = future.await;
 
             let rt_lock = runtime.inner.lock();
-
             let ctx = Ctx::from_ptr(ctx);
-            let then = ctx.deregister(then).unwrap();
-            let catch = ctx.deregister(catch).unwrap();
 
-            match result {
-                Ok(value) => match value.into_js(ctx) {
-                    Ok(value) => schedule_resolution(ctx, then, value),
-                    Err(error) => schedule_resolution(ctx, catch, error.to_string()),
-                },
-                Err(error) => schedule_resolution(ctx, catch, error.to_string()),
+            match result.into_js(ctx) {
+                Ok(value) => {
+                    mem::drop(catch);
+                    resolve(ctx, then.inlive().restore(ctx).unwrap(), value)
+                }
+                Err(error) => {
+                    mem::drop(then);
+                    resolve(
+                        ctx,
+                        catch.inlive().restore(ctx).unwrap(),
+                        error.into_js(ctx).unwrap(),
+                    )
+                }
             };
 
             mem::drop(rt_lock);
@@ -135,20 +139,30 @@ where
     }
 }
 
-fn schedule_resolution<'js, V: IntoJs<'js>>(ctx: Ctx<'js>, func: Value<'js>, val: V) {
-    if let Ok(val) = val.into_js(ctx) {
-        let args = [func.as_js_value(), val.as_js_value()];
-        unsafe {
-            qjs::JS_EnqueueJob(
-                ctx.ctx,
-                Some(resolution_job),
-                args.len() as i32,
-                args.as_ptr() as *mut _,
-            );
-        }
+#[cfg(not(feature = "deferred-resolution"))]
+fn resolve<'js>(_ctx: Ctx<'js>, func: Function<'js>, value: Value<'js>) {
+    if let Err(error) = func.call::<_, Value>((value,)) {
+        eprintln!("Error when promise resolution: {}", error);
     }
 }
 
+#[cfg(feature = "deferred-resolution")]
+fn resolve<'js>(ctx: Ctx<'js>, func: Function<'js>, value: Value<'js>) {
+    let args = [func.0.as_js_value(), value.as_js_value()];
+    unsafe {
+        qjs::JS_EnqueueJob(
+            ctx.ctx,
+            Some(resolution_job),
+            args.len() as _,
+            args.as_ptr() as _,
+        );
+    }
+    mem::drop(args);
+    mem::drop(func);
+    mem::drop(value);
+}
+
+#[cfg(feature = "deferred-resolution")]
 unsafe extern "C" fn resolution_job(
     ctx: *mut qjs::JSContext,
     argc: qjs::c_int,
@@ -159,4 +173,87 @@ unsafe extern "C" fn resolution_job(
     let argv = argv.offset(1);
     let argc = argc - 1;
     qjs::JS_Call(ctx, func, this, argc, argv)
+}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[cfg(feature = "async-std")]
+    use async_std_rs::task::block_on;
+    #[cfg(feature = "tokio")]
+    fn block_on<F: std::future::Future>(future: F) -> F::Output {
+        tokio_rs::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap()
+            .block_on(future)
+    }
+
+    #[test]
+    fn async_fn_no_throw() {
+        block_on(async {
+            async fn mul2(a: i32, b: i32) -> i32 {
+                a * b
+            }
+
+            let rt = Runtime::new().unwrap();
+            let ctx = Context::full(&rt).unwrap();
+
+            rt.spawn_pending_jobs(false);
+
+            let res: Promise<i32> = ctx.with(|ctx| {
+                let global = ctx.globals();
+                global
+                    .set("mul2", JsFn::new("mul2", |a, b| PromiseJs(mul2(a, b))))
+                    .unwrap();
+                ctx.eval("mul2(2, 3)").unwrap()
+            });
+
+            let res = res.await.unwrap();
+            assert_eq!(res, 6);
+        });
+    }
+
+    #[test]
+    #[ignore] // TODO:
+    fn async_fn_unhandled_promise() {
+        block_on(async {
+            async fn doit() {}
+
+            let rt = Runtime::new().unwrap();
+            let ctx = Context::full(&rt).unwrap();
+
+            rt.spawn_pending_jobs(false);
+
+            ctx.with(|ctx| {
+                let global = ctx.globals();
+                global
+                    .set("doit", JsFn::new("doit", || PromiseJs(doit())))
+                    .unwrap();
+                let _ = ctx.eval::<Value, _>("doit()").unwrap();
+            });
+        });
+    }
+
+    #[test]
+    #[ignore] // TODO:
+    fn async_fn_unhandled_promise_future() {
+        block_on(async {
+            async fn doit() {}
+
+            let rt = Runtime::new().unwrap();
+            let ctx = Context::full(&rt).unwrap();
+
+            rt.spawn_pending_jobs(false);
+
+            let _res: Promise<()> = ctx.with(|ctx| {
+                let global = ctx.globals();
+                global
+                    .set("doit", JsFn::new("doit", || PromiseJs(doit())))
+                    .unwrap();
+                ctx.eval("doit()").unwrap()
+            });
+        });
+    }
 }
