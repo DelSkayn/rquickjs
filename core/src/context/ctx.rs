f520974ce21b955f052e44e1ef9b31b0222280ca
Orne Brocaar <info@brocaar.com>
2022-06-03 17:36:02 +0100

    Add `Ctx::eval_with_options` and `Ctx::eval_file_with_options`. (#65)
    Closes #64.

--- core/src/context/ctx.rs
+++ core/src/context/ctx.rs
@@ -20,6 +20,46 @@ use std::{
     path::Path,
 };
 
+/// Eval options.
+pub struct EvalOptions {
+    /// Global code.
+    pub global: bool,
+    /// Force 'strict' mode.
+    pub strict: bool,
+    /// Don't include the stack frames before this eval in the Error() backtraces.
+    pub backtrace_barrier: bool,
+}
+
+impl EvalOptions {
+    fn to_flag(&self) -> i32 {
+        let mut flag = if self.global {
+            qjs::JS_EVAL_TYPE_GLOBAL
+        } else {
+            qjs::JS_EVAL_TYPE_MODULE
+        };
+
+        if self.strict {
+            flag |= qjs::JS_EVAL_FLAG_STRICT;
+        }
+
+        if self.backtrace_barrier {
+            flag |= qjs::JS_EVAL_FLAG_BACKTRACE_BARRIER;
+        }
+
+        flag as i32
+    }
+}
+
+impl Default for EvalOptions {
+    fn default() -> Self {
+        EvalOptions {
+            global: true,
+            strict: true,
+            backtrace_barrier: false,
+        }
+    }
+}
+
 /// Context in use, passed to [`Context::with`].
 #[derive(Clone, Copy, Debug)]
 pub struct Ctx<'js> {
@@ -55,18 +95,35 @@ impl<'js> Ctx<'js> {
         handle_exception(self, val)
     }
 
-    /// Evaluate a script in global context
+    /// Evaluate a script in global context.
     pub fn eval<V: FromJs<'js>, S: Into<Vec<u8>>>(self, source: S) -> Result<V> {
+        self.eval_with_options(source, Default::default())
+    }
+
+    /// Evaluate a script with the given options.
+    pub fn eval_with_options<V: FromJs<'js>, S: Into<Vec<u8>>>(
+        self,
+        source: S,
+        options: EvalOptions,
+    ) -> Result<V> {
         let file_name = unsafe { CStr::from_bytes_with_nul_unchecked(b"eval_script\0") };
-        let flag = qjs::JS_EVAL_TYPE_GLOBAL | qjs::JS_EVAL_FLAG_STRICT;
+
         V::from_js(self, unsafe {
-            let val = self.eval_raw(source, file_name, flag as i32)?;
+            let val = self.eval_raw(source, file_name, options.to_flag())?;
             Value::from_js_value(self, val)
         })
     }
 
     /// Evaluate a script directly from a file.
     pub fn eval_file<V: FromJs<'js>, P: AsRef<Path>>(self, path: P) -> Result<V> {
+        self.eval_file_with_options(path, Default::default())
+    }
+
+    pub fn eval_file_with_options<V: FromJs<'js>, P: AsRef<Path>>(
+        self,
+        path: P,
+        options: EvalOptions,
+    ) -> Result<V> {
         let buffer = fs::read(path.as_ref())?;
         let file_name = CString::new(
             path.as_ref()
@@ -75,9 +132,9 @@ impl<'js> Ctx<'js> {
                 .to_string_lossy()
                 .into_owned(),
         )?;
-        let flag = qjs::JS_EVAL_TYPE_GLOBAL | qjs::JS_EVAL_FLAG_STRICT;
+
         V::from_js(self, unsafe {
-            let val = self.eval_raw(buffer, file_name.as_c_str(), flag as i32)?;
+            let val = self.eval_raw(buffer, file_name.as_c_str(), options.to_flag())?;
             Value::from_js_value(self, val)
         })
     }
@@ -193,5 +250,107 @@ mod test {
             let func: Function = module.get("default").unwrap();
             func.call::<(), ()>(()).unwrap();
         });
+    }
+
+    #[test]
+    fn eval() {
+        use crate::{Context, Runtime};
+
+        let runtime = Runtime::new().unwrap();
+        let ctx = Context::full(&runtime).unwrap();
+        ctx.with(|ctx| {
+            let res: String = ctx
+                .eval(
+                    r#"
+                    function test() {
+                        var foo = "bar";
+                        return foo;
+                    }
+
+                    test()
+                "#,
+                )
+                .unwrap();
+
+            assert_eq!("bar".to_string(), res);
+        })
+    }
+
+    #[test]
+    #[should_panic(expected = "'foo' is not defined")]
+    fn eval_with_sloppy_code() {
+        use crate::{Context, Runtime};
+
+        let runtime = Runtime::new().unwrap();
+        let ctx = Context::full(&runtime).unwrap();
+        ctx.with(|ctx| {
+            let _: String = ctx
+                .eval(
+                    r#"
+                    function test() {
+                        foo = "bar";
+                        return foo;
+                    }
+
+                    test()
+                "#,
+                )
+                .unwrap();
+        })
+    }
+
+    #[test]
+    fn eval_with_options_no_strict_sloppy_code() {
+        use crate::{Context, EvalOptions, Runtime};
+
+        let runtime = Runtime::new().unwrap();
+        let ctx = Context::full(&runtime).unwrap();
+        ctx.with(|ctx| {
+            let res: String = ctx
+                .eval_with_options(
+                    r#"
+                    function test() {
+                        foo = "bar";
+                        return foo;
+                    }
+
+                    test()
+                "#,
+                    EvalOptions {
+                        strict: false,
+                        ..Default::default()
+                    },
+                )
+                .unwrap();
+
+            assert_eq!("bar".to_string(), res);
+        })
+    }
+
+    #[test]
+    #[should_panic(expected = "'foo' is not defined")]
+    fn eval_with_options_strict_sloppy_code() {
+        use crate::{Context, EvalOptions, Runtime};
+
+        let runtime = Runtime::new().unwrap();
+        let ctx = Context::full(&runtime).unwrap();
+        ctx.with(|ctx| {
+            let _: String = ctx
+                .eval_with_options(
+                    r#"
+                    function test() {
+                        foo = "bar";
+                        return foo;
+                    }
+
+                    test()
+                "#,
+                    EvalOptions {
+                        strict: true,
+                        ..Default::default()
+                    },
+                )
+                .unwrap();
+        })
     }
 }
