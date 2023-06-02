e1a33f0fe6eb4f9530f5522446c4795b5bea184c
delskayn <mees.delzenne@gmail.com>
2023-06-02 10:01:05 +0200

    Added some more compile tests

--- core/tests/async_compile_fail/async_nested_contexts.rs
+++ core/tests/async_compile_fail/async_nested_contexts.rs
@@ -6,6 +6,8 @@ pub async fn test() {
     let ctx_2 = AsyncContext::full(&rt).await.unwrap();
     async_with!(ctx_1 => |ctx_1|{
         async_with!(ctx_2 => |ctx_2|{
+            // It is disallowed to use multiple ctx object together from different with closures.
+            // Lifetime on ctx should be unique.
             ctx_1.globals().set("t", ctx_2.globals());
         }).await
     })
--- core/tests/compile_fail.rs
+++ core/tests/compile_fail.rs
@@ -5,4 +5,6 @@ fn compile_test() {
     t.compile_fail("tests/compile_fail/*.rs");
     #[cfg(feature = "futures")]
     t.compile_fail("tests/async_compile_fail/*.rs");
+    #[cfg(all(feature = "futures", feature = "parallel"))]
+    t.compile_fail("tests/async_parallel_compile_fail/*.rs");
 }
