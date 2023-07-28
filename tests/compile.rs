4b23f8fa2411cf3da76bce19860994d73794d1ad
Mees Delzenne <mees.delzenne@gmail.com>
2023-07-28 13:00:27 +0200

    Disable compile syntex error tests

--- tests/compile.rs
+++ tests/compile.rs
@@ -3,9 +3,10 @@
 fn macros() {
     let t = trybuild::TestCases::new();
     t.pass("tests/macros/pass_*.rs");
+    #[cfg(feature = "compile_tests")]
     t.compile_fail("tests/compile_fail/*.rs");
-    #[cfg(feature = "futures")]
+    #[cfg(all(feature = "futures", feature = "compile_tests"))]
     t.compile_fail("tests/async_compile_fail/*.rs");
-    #[cfg(all(feature = "futures", feature = "parallel"))]
+    #[cfg(all(feature = "futures", feature = "parallel", feature = "compile_tests"))]
     t.compile_fail("tests/async_parallel_compile_fail/*.rs");
 }
