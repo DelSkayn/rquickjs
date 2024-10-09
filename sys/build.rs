5dd64cc5df11ce2028c527a27c1df5a386764b3d
Richard Davison <ridaviso@amazon.com>
2024-10-09 11:00:01 +0200

    Test just enable atomics and set c to be 11

--- sys/build.rs
+++ sys/build.rs
@@ -160,31 +160,33 @@ fn main() {
 
     let mut bindgen_cflags = vec![];
 
-    if target_os == "windows" && target_env == "msvc" {
+    if target_os == "windows" {
+        // && target_env == "msvc" {
         let msvc_build_flags = vec![
-            "-Wno-unsafe-buffer-usage",
-            "-Wno-sign-conversion",
-            "-Wno-nonportable-system-include-path",
-            "-Wno-implicit-int-conversion",
-            "-Wno-shorten-64-to-32",
-            "-Wno-reserved-macro-identifier",
-            "-Wno-reserved-identifier",
-            "-Wdeprecated-declarations",
+            "/std:c11",
+            // "-Wno-unsafe-buffer-usage",
+            // "-Wno-sign-conversion",
+            // "-Wno-nonportable-system-include-path",
+            // "-Wno-implicit-int-conversion",
+            // "-Wno-shorten-64-to-32",
+            // "-Wno-reserved-macro-identifier",
+            // "-Wno-reserved-identifier",
+            // "-Wdeprecated-declarations",
             "/experimental:c11atomics",
-            "/wd4018", // -Wno-sign-conversion
-            "/wd4061", // -Wno-implicit-fallthrough
-            "/wd4100", // -Wno-unused-parameter
-            "/wd4200", // -Wno-zero-length-array
-            "/wd4242", // -Wno-shorten-64-to-32
-            "/wd4244", // -Wno-shorten-64-to-32
-            "/wd4245", // -Wno-sign-compare
-            "/wd4267", // -Wno-shorten-64-to-32
-            "/wd4388", // -Wno-sign-compare
-            "/wd4389", // -Wno-sign-compare
-            "/wd4710", // Function not inlined
-            "/wd4711", // Function was inlined
-            "/wd4820", // Padding added after construct
-            "/wd5045", // Compiler will insert Spectre mitigation for memory load if /Qspectre switch specified
+            // "-Wno-sign-conversion",
+            // "-Wno-implicit-fallthrough",
+            // "/wd4100", // -Wno-unused-parameter
+            // "/wd4200", // -Wno-zero-length-array
+            // "/wd4242", // -Wno-shorten-64-to-32
+            // "/wd4244", // -Wno-shorten-64-to-32
+            // "/wd4245", // -Wno-sign-compare
+            // "/wd4267", // -Wno-shorten-64-to-32
+            // "/wd4388", // -Wno-sign-compare
+            // "/wd4389", // -Wno-sign-compare
+            // "/wd4710", // Function not inlined
+            // "/wd4711", // Function was inlined
+            // "/wd4820", // Padding added after construct
+            // "/wd5045", // Compiler will insert Spectre mitigation for memory load if /Qspectre switch specified
         ];
 
         for flag in msvc_build_flags {
