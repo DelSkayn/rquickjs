d047eebacea2271049ba6eee55a11596b83b3372
Richard Davison <ridaviso@amazon.com>
2024-10-10 11:10:16 +0200

    Debug build target

--- sys/build.rs
+++ sys/build.rs
@@ -304,7 +304,7 @@ where
         println!(
             "cargo:warning=rquickjs probably doesn't ship bindings for platform `{}({})`. try the `bindgen` feature instead.",
             target,
-            env::var("CARGO_TARGET_TMPDIR").unwrap_or("n/a".into())
+            env::var("BUILD_TARGET").unwrap_or("n/a".into())
         );
     }
 
