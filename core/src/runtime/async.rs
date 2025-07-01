dc22c719ee6baf3f02b84d06c8d1e0f5810b5fb6
Richard Davison <richard.davison1@gmail.com>
2025-07-01 06:52:12 +0200

    Format

--- core/src/runtime/async.rs
+++ core/src/runtime/async.rs
@@ -13,14 +13,14 @@ use std::sync::mpsc::{self, Receiver, Sender};
 use async_lock::Mutex;
 
 use super::{
-    opaque::Opaque, raw::RawRuntime, schedular::SchedularPoll, spawner::DriveFuture,
-    InterruptHandler, MemoryUsage, RejectionTracker, PromiseHook,
+    InterruptHandler, MemoryUsage, PromiseHook, RejectionTracker, opaque::Opaque, raw::RawRuntime,
+    schedular::SchedularPoll, spawner::DriveFuture,
 };
 use crate::allocator::Allocator;
 #[cfg(feature = "loader")]
 use crate::loader::{Loader, Resolver};
 use crate::{
-    context::AsyncContext, result::AsyncJobException, util::ManualPoll, Ctx, Exception, Result,
+    Ctx, Exception, Result, context::AsyncContext, result::AsyncJobException, util::ManualPoll,
 };
 #[cfg(feature = "parallel")]
 use crate::{
@@ -170,7 +170,7 @@ impl AsyncRuntime {
                 .await
                 .runtime
                 .set_host_promise_rejection_tracker(tracker);
-       }
+        }
     }
 
     /// Set a closure which is called when a promise is created, resolved, or chained.
@@ -325,7 +325,7 @@ impl AsyncRuntime {
                 match pending {
                     Err(e) => {
                         // SAFETY: Runtime is already locked so creating a context is safe.
-                        let ctx = unsafe { Ctx::from_ptr(e.0 .0.ctx().as_ptr()) };
+                        let ctx = unsafe { Ctx::from_ptr(e.0.0.ctx().as_ptr()) };
                         let err = ctx.catch();
                         if let Some(_x) = err.clone().into_object().and_then(Exception::from_object)
                         {
